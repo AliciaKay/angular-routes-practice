c7c55a7cc06c30226fa8dbb2b367325fc2f72040
Alicia Wilking <aliciakaywilking@gmail.com>
2017-09-26 11:22:49 -0500

    completed base testing functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,16 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function($routeProdiver) {  
-    $routeProvider.when('/', {  
-    templateUrl: 'views/home.html',
-    controller: 'HomeController as HC'
-}).otherwise({redirectTo: '/'});
-});
+myApp.config(function($routeProdiver) {
+    $routeProvider.when('/', {                    
+       templateUrl: 'views/home.html',
+       controller: 'HomeController as HC'
+   }).when('/controllerA', {
+       templateUrl: 'views/controllerA.html',
+       controller: 'ControllerA as CA'
+   }).when('/controllerB', {
+       templateUrl: 'views/controllerB.html',
+       controller: 'Controllerb as CB'
+   }).otherwise({redirectTo: '/'});
+   
+    $locationProvider.html5Mode(true);
+   });
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.use(express.static('public'));
 
 app.listen(3003, function() {
     console.log('on 3003');
+});
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 });
